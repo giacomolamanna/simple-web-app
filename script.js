8c293307a857c6777f0f16c9e554c3ebb336b289
Giacomo Lamanna <97486319+giacomolamanna@users.noreply.github.com>
2025-02-13 00:57:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
-function showMessage() {
-    document.getElementById("message").innerText = "Hai cliccato il pulsante!";
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        alert("Inserisci un task valido!");
+        return;
+    }
+
+    let taskItem = createTaskItem(taskText, "todo");
+    document.getElementById("todoList").appendChild(taskItem);
+    taskInput.value = "";
+}
+
+function createTaskItem(text, state) {
+    let li = document.createElement("li");
+    li.innerHTML = `<span>${text}</span> <div class="task-buttons"></div>`;
+
+    let buttonsDiv = li.querySelector(".task-buttons");
+
+    if (state === "todo") {
+        let btnMoveToInProgress = document.createElement("button");
+        btnMoveToInProgress.innerText = "In Progress";
+        btnMoveToInProgress.onclick = function () {
+            moveToState(li, "inProgressList");
+        };
+        buttonsDiv.appendChild(btnMoveToInProgress);
+    } else if (state === "inProgress") {
+        let btnMoveToDone = document.createElement("button");
+        btnMoveToDone.innerText = "Done";
+        btnMoveToDone.onclick = function () {
+            moveToState(li, "doneList");
+        };
+        buttonsDiv.appendChild(btnMoveToDone);
+    }
+
+    return li;
+}
+
+function moveToState(taskItem, newStateId) {
+    let newTaskItem = createTaskItem(taskItem.querySelector("span").innerText, newStateId);
+    document.getElementById(newStateId).appendChild(newTaskItem);
+    taskItem.remove();
 }
